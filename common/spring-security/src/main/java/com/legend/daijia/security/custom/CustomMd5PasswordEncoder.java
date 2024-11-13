83c42aba10020893435d3f8cbf78b14dfc7099dc
Lee <737795279@qq.com>
2024-11-13 09:29:36 +0800

    [feature]common module

---
+++ common/spring-security/src/main/java/com/legend/daijia/security/custom/CustomMd5PasswordEncoder.java
@@ -0,0 +1,24 @@
+package com.legend.daijia.security.custom;
+
+import com.legend.daijia.common.util.MD5;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>
+ * 密码处理
+ * </p>
+ */
+@Component
+public class CustomMd5PasswordEncoder implements PasswordEncoder {
+
+    public String encode(CharSequence rawPassword) {
+        return MD5.encrypt(rawPassword.toString());
+    }
+
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return encodedPassword.equals(MD5.encrypt(rawPassword.toString()));
+    }
+
+
+}
